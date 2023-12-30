868de750b85a8a60a4523a2c569fc20c85cfd24f
segelbaot <nope>
2023-12-30 13:08:39 +0100

    websockettest: pass ip from cli

--- tools/webtest.py
+++ tools/webtest.py
@@ -2,6 +2,7 @@ import requests
 import json
 from collections import defaultdict
 import base64
+import sys
 
 if __name__ == "__main__":
     """requests.packages.urllib3.connection.HTTPConnection.default_socket_options = [(6,3,1)]
@@ -25,4 +26,4 @@ if __name__ == "__main__":
             frame.append(data&0xff)
             frame.append((data>>8)&0xff)
         print(frame)
-        x = requests.post("http://151.217.109.32/binframe", data = frame, headers={'Content-Type': 'application/octet-stream'})
+        x = requests.post(f"http://{sys.argv[1]}/binframe", data = frame, headers={'Content-Type': 'application/octet-stream'})
