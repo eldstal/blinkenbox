f0bd141bbb74fe580149375a3a138e3606ad9cbd
eldstal <laeder.keps@gmail.com>
2024-01-03 19:42:06 +0100

    File reorganization

---
+++ videoplayer/install.bat
@@ -0,0 +1,12 @@
+mpremote fs cp ..\matrix.py :matrix.py
+mpremote fs cp ..\fb.py :fb.py
+mpremote fs cp ..\util.py :util.py
+mpremote fs cp ..\file.py :file.py
+mpremote fs cp ..\rp_devices.py :rp_devices.py
+mpremote fs cp ..\framebuddy.mpy :framebuddy.mpy
+
+mpremote fs cp video.fk :video.fk
+
+mpremote reset
+timeout /t 2
+mpremote run video.py
